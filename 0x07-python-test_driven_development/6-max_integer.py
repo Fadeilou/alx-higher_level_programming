43fb2ec31a2fc7bd4cfd0dfb4f86984f01cd8634
ALASSANE Fadeilou <68302531+Fadeilou@users.noreply.github.com>
2024-02-11 09:28:54 +0100

    Update 6-max_integer.py

--- 0x07-python-test_driven_development/6-max_integer.py
+++ 0x07-python-test_driven_development/6-max_integer.py
@@ -1,18 +1,38 @@
 #!/usr/bin/python3
-"""Module to find the max integer in a list
+"""Unittest for max_integer([...])
 """
+import unittest
+from max_integer import max_integer
 
+class TestMaxInteger(unittest.TestCase):
+    def test_regular_list(self):
+        """Test with a regular list"""
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+        self.assertEqual(max_integer([1, 3, 4, 2]), 4)
 
-def max_integer(list=[]):
-    """Function to find and return the max integer in a list of integers
-        If the list is empty, the function returns None
-    """
-    if len(list) == 0:
-        return None
-    result = list[0]
-    i = 1
-    while i < len(list):
-        if list[i] > result:
-            result = list[i]
-        i += 1
-    return result
+    def test_empty_list(self):
+        """Test with an empty list"""
+        self.assertIsNone(max_integer([]))
+
+    def test_negative_values(self):
+        """Test with a list containing negative values"""
+        self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
+        self.assertEqual(max_integer([-4, -2, -3, -1]), -1)
+
+    def test_float_values(self):
+        """Test with a list containing float values"""
+        self.assertEqual(max_integer([1.5, 2.5, 3.5, 4.5]), 4.5)
+        self.assertEqual(max_integer([1.5, 3.5, 4.5, 2.5]), 4.5)
+
+    def test_duplicate_values(self):
+        """Test with a list containing duplicate values"""
+        self.assertEqual(max_integer([4, 4, 4, 4]), 4)
+        self.assertEqual(max_integer([1, 1, 1, 1]), 1)
+
+    def test_mixed_values(self):
+        """Test with a list containing mixed types"""
+        self.assertEqual(max_integer([1, 2, "3", 4]), 4)
+        self.assertEqual(max_integer([1, 3, 4.5, 2]), 4.5)
+
+if __name__ == '__main__':
+    unittest.main()
