726a82bcfeb59c34e92debe2b556104984733b2d
ALASSANE Fadeilou <68302531+Fadeilou@users.noreply.github.com>
2024-02-11 09:40:39 +0100

    Update 6-max_integer.py

--- 0x07-python-test_driven_development/6-max_integer.py
+++ 0x07-python-test_driven_development/6-max_integer.py
@@ -1,19 +1,38 @@
 #!/usr/bin/python3
-"""Module to find the max integer in a list
+"""Unittest for max_integer([...])
 """
+import unittest
+from max_integer import max_integer
 
-def max_integer(lst=None):
-    """Function to find and return the max integer in a list of integers
-        If the list is empty, the function returns None
-    """
-    if lst is None:
-        return None
-    if not isinstance(lst, list):
-        raise TypeError("list must be a list")
-    if len(lst) == 0:
-        return None
-    result = lst[0]
-    for num in lst[1:]:
-        if num > result:
-            result = num
-    return result
+class TestMaxInteger(unittest.TestCase):
+    def test_regular_list(self):
+        """Test with a regular list"""
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+        self.assertEqual(max_integer([1, 3, 4, 2]), 4)
+
+    def test_empty_list(self):
+        """Test with an empty list"""
+        self.assertIsNone(max_integer([]))
+
+    def test_negative_values(self):
+        """Test with a list containing negative values"""
+        self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
+        self.assertEqual(max_integer([-4, -2, -3, -1]), -1)
+
+    def test_float_values(self):
+        """Test with a list containing float values"""
+        self.assertEqual(max_integer([1.5, 2.5, 3.5, 4.5]), 4.5)
+        self.assertEqual(max_integer([1.5, 3.5, 4.5, 2.5]), 4.5)
+
+    def test_duplicate_values(self):
+        """Test with a list containing duplicate values"""
+        self.assertEqual(max_integer([4, 4, 4, 4]), 4)
+        self.assertEqual(max_integer([1, 1, 1, 1]), 1)
+
+    def test_mixed_values(self):
+        """Test with a list containing mixed types"""
+        self.assertEqual(max_integer([1, 2, "3", 4]), 4)
+        self.assertEqual(max_integer([1, 3, 4.5, 2]), 4.5)
+
+if __name__ == '__main__':
+    unittest.main()
